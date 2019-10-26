4bfbd41c493a779b2f001bd40c7b1b48e0a3e85e
nomizo-iox <nomizo.iox@gmail.com>
2019-10-26 13:44:22 -0500

    2nd commit.

---
+++ cmake-build-debug/CMakeFiles/firstC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/firstC.dir/main.c.o"
+  "firstC"
+  "firstC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/firstC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
